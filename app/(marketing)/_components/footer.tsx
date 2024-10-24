d3afb88d038a6f672e0a8f8efa1e124b700c238a
harshit2608 <harshitrock111@gmail.com>
2024-10-25 04:21:16 +0530

    Add portfolio and credit

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,9 +3,21 @@ import Logo from './logo';
 
 const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1f1f1f]">
+    <div className="flex items-center justify-between w-full p-6 bg-background z-50 dark:bg-[#1f1f1f]">
       <Logo />
-      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+      <div className="flex-grow text-center">
+        <Button asChild variant="ghost" size="sm">
+          <a
+            href="https://harshit2608.github.io/portfolio/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Built by Harshit Bargujar
+          </a>
+        </Button>
+      </div>
+
+      <div className="w-full md:w-auto flex justify-end items-center gap-x-2 text-muted-foreground">
         <Button variant="ghost" size="sm">
           Privacy Policy
         </Button>
